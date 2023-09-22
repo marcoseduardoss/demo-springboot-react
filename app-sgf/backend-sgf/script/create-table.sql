c754d8dd159b870806534e3084959f33324351c6
Marcos Eduardo Da Silva Santos <marcos.eduardo@sonda.com>
2023-09-21 23:14:31 -0300

    Primeiro commit

---
+++ app-sgf/backend-sgf/script/create-table.sql
@@ -0,0 +1,8 @@
+-- FuncioNario definition
+
+CREATE TABLE Funcionario (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    valorl TEXT NOT NULL,
+    valor2 TEXT NOT NULL,
+    valor3 TEXT NOT NULL
+);
